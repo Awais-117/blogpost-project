7fb292bc3d8912beec8c4d2609e94981440a8993
Awais <sway808083@gmail.com>
2026-05-16 11:15:22 +0500

    Configured PostgreSQL database

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -13,8 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
-
-
+import dj_database_url
 
 import cloudinary
 import cloudinary.uploader
@@ -121,11 +120,11 @@ WSGI_APPLICATION = 'blogproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL")
+    )
 }
 
 
