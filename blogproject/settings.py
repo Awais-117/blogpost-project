82910a3c1ec8bb9ce0b51c0fa878c902a9cb1f59
Awais <sway808083@gmail.com>
2026-05-16 22:14:19 +0500

    Configured PostgreSQL database

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -122,8 +122,8 @@ WSGI_APPLICATION = 'blogproject.wsgi.application'
 
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv("DATABASE_URL")
+    'default': dj_database_url.parse(
+        os.getenv("DATABASE_URL")
     )
 }
 
